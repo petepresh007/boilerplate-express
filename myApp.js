a408b109f08c9aad82eecb0af304e297e8fbc906
Peter Dozie <peterprecious370@gmail.com>
2023-08-23 02:41:36 -0700

    fixed

--- myApp.js
+++ myApp.js
@@ -7,19 +7,21 @@ console.log("Hello World");
 //     res.send("Hello Express");
 // });
 
-app.use("/public",express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html")
 })
 
 console.log(__dirname)
 
-app.get("/json", (req, res)=>{
-    if (process.env.MESSAGE_STYLE === "uppercase"){
+app.get("/json", (req, res) => {
+    if (process.env.MESSAGE_STYLE === "uppercase") {
         return res.json({ "message": "HELLO JSON" })
+    } else {
+        return res.json({ "message": "Hello json" })
     }
-    res.json({"message": "Hello json"})
+
 })
 
 
